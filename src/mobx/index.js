149468228d0420fa47bc3780222646cff9801592
Amaury Martiny <amaury.martiny@gmail.com>
2018-01-26 11:22:09 +0100

    Add DisplayApp store

--- src/mobx/index.js
+++ src/mobx/index.js
@@ -16,6 +16,7 @@
 
 import ApiStore from './ApiStore';
 import DappsStore from './DappsStore';
+import DisplayAppsStore from './DisplayAppsStore';
 import MethodPermissionsStore from './MethodPermissionsStore';
 import MiddlewareStore from './MiddlewareStore';
 import RequestsStore from './RequestsStore';
@@ -23,6 +24,7 @@ import RequestsStore from './RequestsStore';
 const createRootStore = api => ({
   apiStore: ApiStore.get(api),
   dappsStore: DappsStore.get(api),
+  displayAppsStore: DisplayAppsStore.get(api),
   methodPermissionsStore: MethodPermissionsStore.get(api),
   middlewareStore: MiddlewareStore.get(api),
   requestsStore: RequestsStore.get(api)
