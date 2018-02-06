2193c7b4ba547346ecaf4c068db41efdacfd705c
Amaury Martiny <amaury.martiny@gmail.com>
2018-02-06 16:29:13 +0100

    Do not minify ethereumjs-util

--- config-overrides.js
+++ config-overrides.js
@@ -5,5 +5,9 @@ module.exports = function override(config, env) {
   const rewires = compose(rewireMobx);
   config = rewires(config, env);
 
+  // Don't minify ethereumjs-util
+  // TODO Remove this once https://github.com/ethereumjs/ethereumjs-util/pull/116 is merged
+  config.plugins.splice(3, 1); // Remove the UglifyJsPlugin
+
   return config;
 };
