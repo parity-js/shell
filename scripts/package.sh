7befa12cca1fd331545f5ebf62cb045a20b13e3e
Amaury Martiny <amaury.martiny@gmail.com>
2018-03-07 14:44:51 +0100

    Do not package unless next branch

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-  
+
+BRANCH=$CI_BUILD_REF_NAME
+
 # Build binaries with electron
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then npm run package; fi
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then npm run package -- --mac --win; fi
+if [ "$BRANCH" == "next" ]; then
+  if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then npm run package; fi
+  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then npm run package -- --mac --win; fi
+fi
