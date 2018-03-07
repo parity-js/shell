fddd26e1cd0adda4dd2acd483badfd6ec0ed09a8
Amaury Martiny <amaury.martiny@gmail.com>
2018-03-07 15:28:46 +0100

    Only package binaries on ci-package branch

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-BRANCH=$CI_BUILD_REF_NAME
-
 # Build binaries with electron
-if [ "$BRANCH" == "next" ]; then
+if [ "$TRAVIS_BRANCH" == "ci-package" ]; then
   if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then npm run package; fi
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then npm run package -- --mac --win; fi
 fi
