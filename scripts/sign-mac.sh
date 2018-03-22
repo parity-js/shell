832a4a907f8cff6eb6266e1221cfd2c123f8872d
Amaury Martiny <amaury.martiny@gmail.com>
2018-03-22 14:10:28 +0100

    Fix bug pkg signing

--- scripts/sign-mac.sh
+++ scripts/sign-mac.sh
@@ -4,9 +4,9 @@ set -e # fail on any error
 set -u # treat unset variables as error
 
 cd dist/
-cp Parity\ UI-$VER.dmg Parity-UI.dmg
+cp Parity\ UI-$VER.pkg Parity-UI.pkg
 echo "__________Sign Package__________"
-productsign --sign 'Developer ID Installer: PARITY TECHNOLOGIES LIMITED (P2PX3JU8FT)' Parity-UI.dmg Parity-UI-signed.dmg
+productsign --sign 'Developer ID Installer: PARITY TECHNOLOGIES LIMITED (P2PX3JU8FT)' Parity-UI.pkg Parity-UI-signed.pkg
 mv Parity-UI-signed.pkg "parity-ui_"$VERSION"_macos_x86_64.pkg"
 
 echo "_____ Calculating checksums _____"
