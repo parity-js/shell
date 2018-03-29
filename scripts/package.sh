df0eed1b5a41d24ef88b3bbcc104f9a315976877
Amaury Martiny <amaury.martiny@gmail.com>
2018-03-29 17:42:05 +0200

    Rename package -> release

--- scripts/package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build binaries with electron
-if [ "$TRAVIS_BRANCH" == "ci-package" ]; then
-  if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then npm run package; fi
-  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then npm run package -- --mac --win; fi
-fi
