7565cc6468f4d50834021b6fd412af309c395985
Amaury Martiny <amaury.martiny@gmail.com>
2018-04-26 10:54:48 +0200

    Add progress bar for download

--- src/Connection/connection.js
+++ src/Connection/connection.js
@@ -16,6 +16,7 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Circle } from 'react-progress-bar.js';
 import { FormattedMessage } from 'react-intl';
 import isElectron from 'is-electron';
 import { observer } from 'mobx-react';
@@ -50,6 +51,7 @@ class Connection extends Component {
   state = {
     loading: false,
     parityInstalled: false,
+    progress: 0,
     token: '',
     validToken: false
   }
@@ -63,6 +65,15 @@ class Connection extends Component {
 
     this.setState({ parityInstalled });
 
+    // Listen to messages from main process
+    ipcRenderer.on('parity-download-progress', (_, progress) => {
+      // Run parity once the installation is finished
+      if (progress === 1 && this.state.progress < 1) {
+        setTimeout(() => this.runParity(), 1000);
+      }
+      this.setState({ progress });
+    });
+
     // Run parity if parityInstalled
     if (!parityInstalled) { return; }
 
@@ -74,8 +85,7 @@ class Connection extends Component {
     // TODO Find a more reliable way to know if parity is running or not
     setTimeout(() => {
       if (!this.props.isConnecting) { return; }
-      console.log('Launching parity.');
-      ipcRenderer.send('asynchronous-message', 'run-parity');
+      this.runParity();
     }, 3000);
   }
 
@@ -149,10 +159,21 @@ class Connection extends Component {
 
   renderIcon = () => {
     const { needsToken } = this.props;
-    const { parityInstalled } = this.state;
+    const { parityInstalled, progress } = this.state;
 
     if (needsToken) { return <KeyIcon className={ styles.svg } />; }
-    if (!parityInstalled || !this.isVersionCorrect()) { return <Icon className={ styles.svg } name='warning sign' />; }
+    if (!parityInstalled || progress) {
+      return (
+        <Circle
+          containerStyle={ { height: '100px', marginTop: '-75px', width: '100px' } }
+          initialAnimate
+          options={ { color: 'rgb(208, 208, 208)', strokeWidth: 5 } }
+          progress={ progress }
+          text={ `${Math.round(progress * 100)}%` }
+        />
+      );
+    }
+    if (!this.isVersionCorrect()) { return <Icon className={ styles.svg } name='warning sign' />; }
     return <DashboardIcon className={ styles.svg } />;
   }
 
@@ -225,9 +246,8 @@ class Connection extends Component {
     return (
       <div className={ styles.info }>
         <FormattedMessage
-          id='connection.noParity'
-          defaultMessage="We couldn't find any Parity installation on this computer. If you have it installed, please run it now. If not, please follow the instructions on {link} to install Parity first."
-          values={ { link: <a href='#' onClick={ this.handleOpenWebsite }>https://parity.io</a> } }
+          id='connection.installingParity'
+          defaultMessage='Please wait while we are fetching the latest version of Parity and installing it. This may take a few minutes.'
         />
       </div>
     );
@@ -259,6 +279,13 @@ class Connection extends Component {
         />
       </div>
     );
+  }
+
+  runParity = () => {
+    const { ipcRenderer } = electron;
+
+    console.log('Launching parity.');
+    ipcRenderer.send('asynchronous-message', 'run-parity');
   }
 
   validateToken = (_token) => {
