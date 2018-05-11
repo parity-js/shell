2b634d44a83e51a83c5650782cb0a0c93eec8c09
Amaury Martiny <amaury.martiny@gmail.com>
2018-05-11 10:53:21 +0200

    Change help messages

--- electron/operations/runParity.js
+++ electron/operations/runParity.js
@@ -23,7 +23,6 @@ const cli = require('../cli');
 const handleError = require('./handleError');
 const parityPath = require('../util/parityPath');
 
-const [, parityArgv] = cli();
 const fsExists = util.promisify(fs.stat);
 const fsReadFile = util.promisify(fs.readFile);
 const fsUnlink = util.promisify(fs.unlink);
@@ -39,7 +38,8 @@ module.exports = {
       .then(() => fsUnlink(logFile))
       .catch(() => { })
       .then(() => {
-        var logStream = fs.createWriteStream(logFile, { flags: 'a' });
+        const logStream = fs.createWriteStream(logFile, { flags: 'a' });
+        const parityArgv = cli()[1];
 
         // Run an instance of parity if we receive the `run-parity` message
         parity = spawn(
