3db6c5fe0f70f8230610a1aaf9a9749bfe8d155f
Axel Chalon <xaxel@protonmail.com>
2018-06-13 19:38:01 +0200

    Fix dapp icon URL

--- src/util/dapps.js
+++ src/util/dapps.js
@@ -165,13 +165,15 @@ export function fetchLocalApps (api) {
     .then(dapps =>
       dapps.filter(({ manifest }) => manifest))
     .then(dapps =>
-      dapps.map(({ filename, manifest: { id, localUrl, ...rest } }) => (
+      dapps.map(({ filename, manifest: { id, localUrl, iconUrl, ...rest } }) => (
         {
           ...rest,
           type: 'local',
-          id: `LOCAL-${id}`, // Prevent appId spoofing
+          // Prevent using the appId of an existing non-local dapp with already approved permissions
+          id: `LOCAL-${id}`,
           visible: true,
-          localUrl: localUrl || `file://${dappsPath}/${filename}/index.html`
+          localUrl: localUrl || `file://${dappsPath}/${filename}/index.html`,
+          image: `file://${dappsPath}/${filename}/${iconUrl}`
         }
     )))
     .catch((error) => {
